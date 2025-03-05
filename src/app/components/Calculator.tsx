7c259b15464b7e24876f6ff9333087dc4c190df1
Randy Binondo <randy.binondo@vertexbizapp.com>
2025-03-05 16:18:30 +0800

    revert changes

--- src/app/components/Calculator.tsx
+++ src/app/components/Calculator.tsx
@@ -1,7 +1,7 @@
 export default function Calculator(num: number[], operation: string = 'add') {
 
   if (operation == 'add') {
-    return num.reduce( (acc, n) => acc - n, 0 )
+    return num.reduce( (acc, n) => acc + n, 0 )
   }
   /*
   let result = 0
